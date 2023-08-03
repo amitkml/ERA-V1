1f93c0a6b9c6ad10946dc2fe0c36ff48cd362462
AmitK <49492030+amitkml@users.noreply.github.com>
2023-08-03 13:06:22 +0530

    Update transforms.py

--- Session12/utils/transforms.py
+++ Session12/utils/transforms.py
@@ -101,46 +101,46 @@ class CIFAR10DataModule(pl.LightningDataModule):
         return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=4)
     
 
-# def apply_transforms(mean, std):
-#     """
-#     Image augmentations for train and test set.
-#     """
-#     train_transforms = A.Compose(
-#         [
-#             A.Sequential(
-#                 [
-#                     A.PadIfNeeded(min_height=40, min_width=40, always_apply=True),
-#                     A.RandomCrop(width=32, height=32, p=1),
-#                 ],
-#                 p=0.5,
-#             ),
-#             A.Rotate(limit=5, p=0.2),
-#             A.CoarseDropout(
-#                 max_holes=1,
-#                 max_height=16,
-#                 max_width=16,
-#                 min_holes=1,
-#                 min_height=16,
-#                 min_width=16,
-#                 fill_value=tuple((x * 255.0 for x in mean)),
-#                 p=0.2,
-#             ),
-#             A.Normalize(mean=mean, std=std, always_apply=True),
-#             ToTensorV2(),
-#         ]
-#     )
+def apply_transforms(mean, std):
+    """
+    Image augmentations for train and test set.
+    """
+    train_transforms = A.Compose(
+        [
+            A.Sequential(
+                [
+                    A.PadIfNeeded(min_height=40, min_width=40, always_apply=True),
+                    A.RandomCrop(width=32, height=32, p=1),
+                ],
+                p=0.5,
+            ),
+            A.Rotate(limit=5, p=0.2),
+            A.CoarseDropout(
+                max_holes=1,
+                max_height=16,
+                max_width=16,
+                min_holes=1,
+                min_height=16,
+                min_width=16,
+                fill_value=tuple((x * 255.0 for x in mean)),
+                p=0.2,
+            ),
+            A.Normalize(mean=mean, std=std, always_apply=True),
+            ToTensorV2(),
+        ]
+    )
 
-#     test_transforms = A.Compose(
-#         [
-#             A.Normalize(mean=mean, std=std, always_apply=True),
-#             ToTensorV2(),
-#         ]
-#     )
+    test_transforms = A.Compose(
+        [
+            A.Normalize(mean=mean, std=std, always_apply=True),
+            ToTensorV2(),
+        ]
+    )
 
-#     return (
-#         lambda img: train_transforms(image=np.array(img))["image"],
-#         lambda img: test_transforms(image=np.array(img))["image"],
-#     )
+    return (
+        lambda img: train_transforms(image=np.array(img))["image"],
+        lambda img: test_transforms(image=np.array(img))["image"],
+    )
 
 
 # # def apply_transforms_custom_resnet(mean, std):

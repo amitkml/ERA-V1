078ac4c36e1aea01913ec2c9e9bc905d634b8598
AmitK <49492030+amitkml@users.noreply.github.com>
2023-08-03 13:30:21 +0530

    Update custom_resnet.py

--- Session12/models/custom_resnet.py
+++ Session12/models/custom_resnet.py
@@ -69,7 +69,7 @@ class CustomResNetClass(pl.LightningModule):
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-        self.train_acc = Accuracy()
+        self.train_acc = Accuracy(num_classes=num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
